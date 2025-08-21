fbd3f4ac0049938a3a751c4813b1230dfcc301b7
sandoya <sandoya1101@gmail.com>
2025-08-21 13:08:53 +0300

    lol1

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,74 @@
 import { useState } from "react";
 import './App.css'
-import { Button, Col, Flex, Row, Spin, Tag, Typography } from 'antd';
+import { Button, Col, Modal, Row, Spin, Input, Typography } from 'antd';
 import useBreakpoint from "antd/es/grid/hooks/useBreakpoint";
+import { PlusOutlined } from '@ant-design/icons'
 
 const { Title } = Typography;
 
+const colors = ["green", "red", "blue", "purple"];
+
 const App = () => {
     const [mainTable, setMainTable] = useState(false);
+    const [blocks, setBlocks] = useState([
+        { id: 1, title: 'Bio', content: ['Sergey', 'Age: 26'] },
+        { id: 2, title: 'About me', content: ['lalala dota player and lorem'] },
+        { id: 3, title: 'Skills', content: ['professional swimmer', 'life enjoyer'] }
+    ]);
+    const [modal, setModal] = useState(false);
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
     const screens = useBreakpoint();
 
+    const showModal = () => setModal(true);
+
+    const handleCancel = () => {
+        setModal(false);
+        setTitle('');
+        setContent('');
+    };
+
+    const handleAddBlock = () => {
+        if (!title || !content) return;
+
+        const newBlock = {
+            id: Date.now(),
+            title: title,
+            content: content.split(',') // превращаем строку в массив
+        };
+        setBlocks([...blocks, newBlock]);
+        setModal(false);
+        setTitle('');
+        setContent('');
+    };
+
     return (
-        <div className="App">
+        <div className='App'>
             {mainTable && (
                 <div>
-                    <Row gutter={[16, 16]} justify={'center'} className={'table'}>
-                        <Col xs={24} md={8} className={'bio'}>
-                            <Title level={4}>Name: Sergey</Title>
-                            <Title level={5}>Age: 26</Title>
-                        </Col>
-                        <Col xs={24} md={{span: 16,order:3}} className={'about'} >
-                            <Title level={4}>About me: </Title>
-                            <Title level={5}>lalalala dota player and lorem ipsum</Title>
-                        </Col>
-                        <Col xs={24} md={{span: 12, order:2}} className={'skills'} >
-                            <Title level={4}>Skills:</Title>
-                            <Flex align={'center'} gap="16px" vertical={true} className={'tags'}>
-                                <Tag bordered={false} color="processing">professional swimmer</Tag>
-                                <Tag bordered={false} color="cyan">life enjoyer</Tag>
-                                <Tag bordered={false} color="lime">bruskett' eater</Tag>
-                            </Flex>
-                        </Col>
+                    <Button type='dashed' icon={<PlusOutlined />} size='large' block={screens.xs} onClick={showModal}></Button>
+                    <Row gutter={[16, 16]} justify='center' className='table'>
+                        {blocks.map((block, index) => (
+                            <Col xs={24} md={12} key={block.id} className='block'
+                                style={{ backgroundColor: colors[index % colors.length]}}>
+                                <Title level={3}>{block.title}</Title>
+                                {block.content.map((item, i) => (
+                                    <Title level={5} key={i}>{item}</Title>
+                                ))}
+                            </Col>
+                        ))}
                     </Row>
                 </div>
             )}
-                    <Button size={"large"} onClick={() => setMainTable(!mainTable)}
-                    block={screens.xs}>
-                        {!mainTable ? <Spin size="small" /> : "Hide"}
-                    </Button>
+
+            <Modal title='Add new block' open={modal} onOk={handleAddBlock} onCancel={handleCancel} okText='Add' cancelText='Cancel'>
+                <Input placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)}/>
+                <Input placeholder='Content' onChange={(e) => setContent(e.target.value)} value={content}/>
+            </Modal>
+
+            <Button size='large' onClick={() => setMainTable(!mainTable)} block={screens.xs}>
+                {!mainTable ? <Spin size='small' /> : 'Hide'}
+            </Button>
         </div>
     );
 };
